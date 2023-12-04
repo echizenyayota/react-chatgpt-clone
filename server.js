7589d15c5b4dc135f2d6bdfaa2a8fd481064df4a
echizenyayota <virtualmisso30@gmail.com>
2023-12-04 16:42:37 +0900

    30:37 Set the route to call the API data in the backend

--- server.js
+++ server.js
@@ -11,10 +11,34 @@ const API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
 
 console.log("Hi!");
 
-app.listen(PORT, () => console.log("Your server is running on PORT" + PORT));
-
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Testing whether the API works"
   });
 });
+
+app.post('/completions', async(req, res) => {
+  const options = {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: "How are you?" }],
+      max_tokens: 100, 
+    }),
+  };
+
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.listen(PORT, () => console.log("Your server is running on PORT" + PORT));
+
